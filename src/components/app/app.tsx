87a4be0202edf875221d3c621aa67988ac303fe0
Илья Ватютов <vatut007@ya.ru>
2024-01-04 15:32:27 +0300

    add react-dom

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,31 +1,14 @@
 import styles from "./app.module.css";
-import { AppHeader } from "../app-header/app-header";
-import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
-import { BurgerConstructor } from "../burger-constructor/burger-constructor";
-import { Loading } from "../loading/loadng";
-import { Error } from "../error/error";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { useGetAllIngredientQuery } from "../../services/api/api-slice";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from "../../pages/home-page/home-page";
 
 function App() {
-  const { data, error, isLoading } = useGetAllIngredientQuery(undefined);
-  if (isLoading) {
-    return <Loading />;
-  }
-  if (error || !data) {
-    return <Error error={String(error)} />;
-  }
-  return (
-    <div className={styles.App}>
-      <AppHeader />
-      <DndProvider backend={HTML5Backend}>
-        <main className={styles.main}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </main>
-      </DndProvider>
-    </div>
-  );
+  return(
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </Router>
+  )
 }
 export default App;
