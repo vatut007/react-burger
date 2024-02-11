81594c88d13bf839232480ba64e24d41debfa820
vatut007 <vatut007@ya.ru>
2024-02-11 11:52:25 +0300

    Add link in app header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -14,16 +14,20 @@ export function AppHeader() {
   return (
     <header className={styles.header}>
       <nav>
+      <Link to="/">
         <span className={styles.icon}>
           <BurgerIcon type="primary" />
         </span>{" "}
         Конструктор
+      </Link>
       </nav>
       <nav>
+      <Link to='/feed'>
         <span className={styles.icon}>
           <ListIcon type="primary" />
         </span>
         Лента заказов
+      </Link>
       </nav>
       <Link to="/">
         <Logo />
